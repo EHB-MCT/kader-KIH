efdba17f617e742961853b76436c70accbd64e8b
Ali Amrani <ali.amrani@student.ehb.be>
2023-01-25 16:23:36 +0100

    Working prototype kader

--- src/index.js
+++ src/index.js
@@ -1,21 +1,102 @@
-const { io } = require("socket.io-client");
+const { io } = require('../node_modules/socket.io/client-dist/socket.io.js')
+var socket = io('https://dimetrondon-backend.onrender.com/');
+const THREE = require('three');
+import {
+    GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-let domainURL = "https://localhost:5501";
-const socket = io(domainURL);
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js';
+
+socket.on('display', (object) => {
+    document.getElementById('container').innerHTML = ""
+    console.log(object[0].genre)
+
+    if (object[0].genre == "Video") {
+        let source = document.createElement("source")
+        source.src = "https://dimetrodon.fr/files/" + object[0].file;
+        source.type = "video/mp4"
+        let test = document.createElement('video');
+        test.classList.add('test')
+        test.autoplay = true;
+        test.loop = true;
+        test.load();
+        test.append(source)
+        document.getElementById('container').appendChild(test)
+
+    } else if (object[0].genre == "3D") {
+        let model;
+        let MODEL_ROTATION = 0.002;
+        let LINE_COLOR = 0x000000;
 
 
-socket.on("connect", () => {
-    const engine = socket.io.engine;
-    console.log(engine.transport.name); // in most cases, prints "polling"
+        const scene = new THREE.Scene();
+        const color2 = new THREE.Color(0x000000);
+        scene.background = color2;
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    // engine.once("upgrade", () => {
-    //   // called when the transport is upgraded (i.e. from HTTP long-polling to WebSocket)
-    //   console.log(engine.transport.name); // in most cases, prints "websocket"
-    // });
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('container').appendChild(renderer.domElement);
 
-    engine.on("instruction", ({ type, data }) => {
-        // called for each packet received
-        console.log(data);
 
-    });
-})
+        const geometry = new THREE.BoxGeometry(window.innerWidth / 100, window.innerHeight / 100, 10);
+        const edges = new THREE.EdgesGeometry(geometry);
+        const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({
+            color: LINE_COLOR,
+            linewidth: 0.1
+        }));
+
+        line.translateY(2);
+        scene.add(line);
+
+        camera.position.z = 6;
+        camera.position.y = 2;
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+
+        const loader = new GLTFLoader();
+        loader.load("http://dimetrd.cluster027.hosting.ovh.net/files/" + object[0].file, (gltf) => {
+            model = gltf.scene;
+            scene.add(model);
+            model.scale.set(0.09, 0.09, 0.09);
+
+
+            //light
+            const light1 = new THREE.PointLight('lightblue', 2);
+            light1.position.set(3, 10, 25);
+            const lightHolder = new THREE.Group();
+            lightHolder.add(light1);
+
+
+
+            scene.add(lightHolder);
+            const sphereSize = 1;
+            const pointLightHelper = new THREE.PointLightHelper(light1, sphereSize);
+            scene.add(pointLightHelper);
+
+            function animate() {
+                requestAnimationFrame(animate);
+                model.rotation.y += MODEL_ROTATION;
+                /*     cube.rotation.x += 0.01;
+                    cube.rotation.y += 0.01; */
+
+                renderer.render(scene, camera);
+
+            };
+
+            animate();
+        });
+
+
+
+
+    } else {
+        let test = document.createElement('img');
+        test.classList.add('test')
+        test.src = "https://dimetrodon.fr/files/" + object[0].file;
+        document.getElementById('container').appendChild(test)
+    }
+
+});
